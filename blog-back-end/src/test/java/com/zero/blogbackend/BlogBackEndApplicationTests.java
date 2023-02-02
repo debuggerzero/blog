9075b1e7cec0adfa7687e6dda54a2739ced69d09
zero <debuggerzero@gmail.com>
2023-02-02 23:22:03 +0800

    blog-back-end update

--- blog-back-end/src/test/java/com/zero/blogbackend/BlogBackEndApplicationTests.java
+++ blog-back-end/src/test/java/com/zero/blogbackend/BlogBackEndApplicationTests.java
@@ -1,15 +1,25 @@
 package com.zero.blogbackend;
 
 import com.zero.blogbackend.model.command.LoginCommand;
+import com.zero.blogbackend.model.common.Paging;
+import com.zero.blogbackend.model.po.ArticleInfo;
 import com.zero.blogbackend.model.po.UserInfo;
+import com.zero.blogbackend.model.query.ArticleQuery;
+import com.zero.blogbackend.model.vo.ArticleInfoVO;
 import com.zero.blogbackend.model.vo.UserInfoVO;
+import com.zero.blogbackend.repo.ArticleRepo;
 import com.zero.blogbackend.repo.UserInfoRepo;
+import com.zero.blogbackend.repo.mapper.ArticleMapper;
+import com.zero.blogbackend.service.ArticleService;
+import com.zero.blogbackend.service.CosService;
 import com.zero.blogbackend.service.UserInfoService;
+import com.zero.blogbackend.utils.StringUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.DigestUtils;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootTest
 class BlogBackEndApplicationTests {
@@ -38,6 +48,21 @@ class BlogBackEndApplicationTests {
         loginCommand.setPassword(password);
         UserInfoVO zero = userInfoService.login(loginCommand);
         System.out.println(zero);
+    }
+
+    @Resource
+    private ArticleMapper articleMapper;
+
+    @Test
+    public void testGetArticles() {
+        var articles = articleMapper.queryArticles(null);
+        System.out.println(articles);
+    }
+
+    @Test
+    public void testTotalArticles() {
+        Long aLong = articleMapper.totalArticleCount(1002, 0);
+        System.out.println(aLong);
     }
 
 }
