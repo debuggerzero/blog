90fb7d495db335010131b390f4793fabca67ebea
zero <debuggerzero@gmail.com>
2023-02-02 23:19:23 +0800

    blog-back-end update LoginController

--- blog-back-end/src/main/java/com/zero/blogbackend/controller/LoginController.java
+++ blog-back-end/src/main/java/com/zero/blogbackend/controller/LoginController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/login")
-public class UserInfoController {
+public class LoginController {
 
     private final UserInfoService userInfoService;
 
